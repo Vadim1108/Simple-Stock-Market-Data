e00e5095c078dcfebdca1eb2a03c95b8484cb080
Vadym_K <kononyhin.vadim1@gmail.com>
2022-05-09 18:21:43 +0300

    Made 3 elements + refresh button

--- Simple Stock Market Data/MainWindow.xaml.cs
+++ Simple Stock Market Data/MainWindow.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -23,6 +24,62 @@ namespace Simple_Stock_Market_Data
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        
+        public string HTMLParser (string url, string start, string stop)
+        {
+            var startPosition = url.IndexOf(start);
+            var stopPosition = url.IndexOf(stop, startPosition + start.Length);
+
+            return url.Substring(startPosition + start.Length, stopPosition - startPosition - start.Length);
+        }
+        public string AAPL()
+        {
+            // Line from HTML for data
+            //<div class="YMlKec fxKbKc">153,28&nbsp;$</div>
+            string Start = "YMlKec fxKbKc\">$";
+            string Stop = "</div>";
+
+            WebClient client = new WebClient();
+            string Url = client.DownloadString(@"https://www.google.com/finance/quote/AAPL:NASDAQ");
+            return HTMLParser(Url, Start, Stop);
+        }
+        public string MSFT()
+        {
+            //< div class="YMlKec fxKbKc">266,38&nbsp;$</div>
+            string Start = "YMlKec fxKbKc\">$";
+            string Stop = "</div>";
+
+            WebClient client = new WebClient();
+            string Url = client.DownloadString(@"https://www.google.com/finance/quote/MSFT:NASDAQ");
+            return HTMLParser(Url, Start, Stop);
+        }
+        public string SP500()
+        {
+            //< div class="YMlKec fxKbKc">4&nbsp;016,98</div>
+            string Start = "YMlKec fxKbKc\">";
+            string Stop = "</div>";
+
+            WebClient client = new WebClient();
+            string Url = client.DownloadString(@"https://www.google.com/finance/quote/.INX:INDEXSP");
+            return HTMLParser(Url, Start, Stop);
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+
+            Element1Value.Text = AAPL();
+            Element2Value.Text = MSFT();
+            Element3Value.Text = SP500();
+
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Element1Value.Text = AAPL();
+            Element2Value.Text = MSFT();
+            Element3Value.Text = SP500();
         }
     }
 }
